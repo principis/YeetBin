89e46b8e8edc49b48e001e3f1181435cf4c2136d
Arthur Bols <arthur@bols.dev>
2023-02-18 17:53:11 +0100

    [ServiceResolver] Add support for services with constructors

--- src/ArgumentResolver/ServiceResolver.php
+++ src/ArgumentResolver/ServiceResolver.php
@@ -1,5 +1,23 @@
 <?php
 
+/*
+ * This file is part of YeetBin.
+ * Copyright (C) 2023 Arthur Bols
+ *
+ * YeetBin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * YeetBin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with YeetBin.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 namespace App\ArgumentResolver;
 
 use Symfony\Component\HttpFoundation\Request;
@@ -10,6 +28,8 @@ class ServiceResolver implements ValueResolverInterface
 {
 
     private array $services;
+
+    private array $instances = [];
 
     public function __construct(array $services)
     {
@@ -23,6 +43,36 @@ class ServiceResolver implements ValueResolverInterface
             return [];
         }
 
-        return [new $class];
+        if (isset($this->instances[$class])) {
+            return [$this->instances[$class]];
+        }
+
+
+        return [$this->resolveServiceArgument($class)];
+    }
+
+    private function resolveServiceArgument($class) :null|object
+    {
+        if (isset($this->instances[$class])) {
+            return $this->instances[$class];
+        }
+
+        $reflect = new \ReflectionClass($class);
+
+        $constructor = $reflect->getConstructor();
+        if ($constructor !== null) {
+            $args = [];
+            foreach ($reflect->getConstructor()->getParameters() as $parameter) {
+                $args[] = $this->resolveServiceArgument($parameter->getType()->getName());
+            }
+
+            $obj = $reflect->newInstanceArgs($args);
+        } else {
+            $obj = $reflect->newInstanceWithoutConstructor();
+        }
+
+        $this->instances[$class] = $obj;
+
+        return $obj;
     }
 }
